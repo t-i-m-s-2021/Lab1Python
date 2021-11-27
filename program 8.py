d5326c02bcabf1a3ecd0d23dab5d9941e9c22666
t-i-m-s-2021 <94782806+t-i-m-s-2021@users.noreply.github.com>
2021-11-27 19:02:11 +0300

    Add files via upload

---
+++ program 8.py
@@ -0,0 +1,22 @@
+import math
+print(" Введите коэффициенты для уравнения ax^2+bx+c=0:")
+a=float(input("a="))
+b=float(input("b="))
+c=float(input("c="))
+if (a == 0):
+    print("Ваше уравнение не является квадратным!")
+else:
+    D = b**2-4*a*c
+    print(f"Дискриминант = {D}")
+    if D>0:
+        x1 = (-b + math.sqrt(D))/(2*a)
+        x2 = (-b - math.sqrt(D))/(2*a)
+        print(f"Корни уравнения: {x1} и {x2}")
+    elif (D==0):
+        x= -b/(2*a)
+        print(f"Корень уравнения: {x}")
+    else:
+        print("Веденное уравнение не имеет корней")
+    
+
+
