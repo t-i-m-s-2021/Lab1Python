336091abfbd24970a31ddc9ef25be11f2eae560a
t-i-m-s-2021 <94782806+t-i-m-s-2021@users.noreply.github.com>
2021-12-12 23:14:51 +0300

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,8 @@
+import random
+
+
+def get_random_sym_from_str(s):
+    return s[random.randint(0, len(s))]
+
+
+
---
+++ homework 7.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import pprint
+from helpers import get_random_sym_from_str
+
+print("Текущее имя системы: {}".format(os.name))
+print("Текущее имя системы: {}".format(os.environ["OS"]))
+
+pprint.pprint(list(os.environ.items()))
+
+print("Текущее имя пользователя: {}".format(os.environ["USERNAME"]))
+print("Текущее имя пользователя: {}".format(os.getlogin()))
+
+for elem in os.walk(os.getcwd()):
+    print("В папке(абсолютный путь)", elem[0])
+    print("Содержатся папки:", *elem[1], sep=" ")
+    print("Содержатся файлы:", *elem[2], sep=" ")
+
+a = np.random.randint(0, 10, (3, 3))
+print(a)
+print(a.transpose())
+
